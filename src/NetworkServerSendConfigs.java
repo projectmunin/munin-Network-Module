089173b4f0ac6bcba9374f03e7d5fdf59535b0cc
pandersson06 <pontus_proffs06@hotmail.com>
2014-03-21 16:14:18 +0100

    Fixed some bugs, Should work. fix #19

--- src/NetworkServerSendConfigs.java
+++ src/NetworkServerSendConfigs.java
@@ -7,18 +7,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+/**
+ * Sends new config to target Raspbarry Pi
+ * 
+ * @author P. Andersson
+ * 
+ */
 public class NetworkServerSendConfigs
 {
 	//Variables
 	static Boolean debugMode;
-	static Boolean dummyMode; //TODO rm 
 	static String rasPiId = "";
 	static String room = "";
 	static String serverPassword = "";
 	static String serverName = "";
 	
-	static String ethernetName = "etho";
+	//Internet connection to use
+	static String ethernetName = "lo";
 	
 	//Database info
 	static String user = "root";
@@ -26,87 +31,80 @@ public class NetworkServerSendConfigs
 	static String URL = "//localhost/munin";
 	
 	/**
-	 * @param args
+	 * Runs the whole program.
+	 * Reads input -> reads database -> create file to be sent -> send file -> close
+	 * @param args Input argumetns
 	 */
 	public static void main(String[] args) 
 	{
 		//Reads arguments
 		readInput(args);
 		getName();
-//		System.out.println(rasPiId);
-//		System.out.println(room);
-//		System.out.println(serverPassword);
-//		System.out.println(serverName);
-//		System.exit(0);
 		
-		if (!dummyMode) //TODO rm
+		//Sets folder and Log class
+		Log log = new Log("/home/panda/test/log/", debugMode);  //Log folder Diffrent log file then Main NetworkServer!!!
+		String tmpNewConfigFolder = "/home/panda/test/tmpNewConfigs/";
+		String targetFolder = ":/home/panda/test/serverInputs/"; //DONT forgett : at start
+		String filePath = tmpNewConfigFolder + rasPiId + "_server_config.xml";
+		
+		//Creates new configfile
+		EncodeDecodeXml configFile = new EncodeDecodeXml(tmpNewConfigFolder, log);
+		configFile.createNewXml(filePath, "config");
+		
+		configFile.addLectureHall(room);
+		configFile.addServerIp(new getCurrentIp().get(ethernetName));
+		configFile.addServerName(serverName);
+		configFile.addServerPassword(serverPassword);
+		
+		//Gets Raspberry Pi:s ip and password from database
+		String rasPiIp = "";
+		String rasPiPassword = "";
+		try
 		{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection connect = DriverManager
+					.getConnection("jdbc:mysql:" + URL + "?user="+ user + "&password=" + password);
 			
-			//Sets folder and Log class
-			Log log = new Log("/home/panda/test/log/", debugMode);  //Log folder Diffrent log file then Main NetworkServer!!!
-			String tmpNewConfigFolder = "/home/panda/test/tmpNewConfigs/";
-			String targetFolder = "/home/panda/test/serverInputs/";
-			String filePath = tmpNewConfigFolder + rasPiId + "_server_config.xml";
-			
-			//Creates new configfile
-			EncodeDecodeXml configFile = new EncodeDecodeXml(tmpNewConfigFolder, log);
-			configFile.createNewXml(filePath, "config");
-			
-			configFile.addLectureHall(room);
-			configFile.addServerIp(new getCurrentIp().get(ethernetName));
-			configFile.addServerName(serverName);
-			configFile.addServerPassword(serverPassword);
-			
-			//Gets Raspberry Pi:s ip and password from database
-			String rasPiIp = "";
-			String rasPiPassword = "";
-			try
+			Statement  statement = connect.createStatement();
+
+			ResultSet result = statement.executeQuery("SELECT ip_address, password " +
+													"FROM camera_units " +
+													"WHERE name = '" + rasPiId + "'");
+			if (result.next())
 			{
-				Class.forName("com.mysql.jdbc.Driver");
-				Connection connect = DriverManager
-						.getConnection("jdbc:mysql:" + URL + "?user="+ user + "&password=" + password);
-				
-				Statement  statement = connect.createStatement();
-	
-				ResultSet result = statement.executeQuery("SELECT ip_address, password " +
-														"FROM camera_units " +
-														"WHERE name = '" + rasPiId + "'");
-				if (result.next())
-				{
-					rasPiIp = result.getString(1);
-					rasPiPassword = result.getString(2);
-					log.write(true, "[SUCCESS] Network-NetworkServerSendCondfig; Read rasPiIp and password from database");
-				}
-				else
-				{
-					log.write(false, "[ERROR] Network-NetworkServerSendCondfig; Cant find a rasPi with ID: " + rasPiId);
-					System.exit(3);
-				}
-			}
-			catch (SQLException e)
-			{
-				log.write(false, "[ERROR] Network-NetworkServerSendCondfig; " + e.getMessage());
-				System.exit(4);
-			}
-			catch (ClassNotFoundException e) 
-			{
-				log.write(false, "[ERROR] Network-NetworkServerSendCondfig; " + e.getMessage());
-				System.exit(4);
-			}
-			
-			//Sends new configs to rasPI
-			if (new NetworkSender(log, rasPiIp, targetFolder, rasPiId, rasPiPassword).trySendingFile(filePath))
-			{
-				log.write(true, "[SUCCESS] Network-NetworkServerSendCondfig; Sent new configs to raspi with ID: " + rasPiId);
-				log.print("Sent new configs to rasPi with ID " + rasPiId);
-				System.exit(0);
+				rasPiIp = result.getString(1);
+				rasPiPassword = result.getString(2);
+				log.write(true, "[SUCCESS] Network-NetworkServerSendCondfig; Read rasPiIp and password from database");
 			}
 			else
 			{
-				log.write(false, "[ERROR] Network-NetworkServerSendCondfig; Could not send file to rasPi with ID: " + rasPiId);
-				log.print("Could not send file to rasPi with ID: " + rasPiId);
-				System.exit(1);
+				log.write(false, "[ERROR] Network-NetworkServerSendCondfig; Cant find a rasPi with ID: " + rasPiId);
+				System.exit(3);
 			}
+		}
+		catch (SQLException e)
+		{
+			log.write(false, "[ERROR] Network-NetworkServerSendCondfig; " + e.getMessage());
+			System.exit(4);
+		}
+		catch (ClassNotFoundException e) 
+		{
+			log.write(false, "[ERROR] Network-NetworkServerSendCondfig; " + e.getMessage());
+			System.exit(4);
+		}
+		
+		//Sends new configs to rasPI
+		if (new NetworkSender(log, rasPiIp, targetFolder, rasPiId, rasPiPassword).trySendingFile(filePath))
+		{
+			log.write(true, "[SUCCESS] Network-NetworkServerSendCondfig; Sent new configs to raspi with ID: " + rasPiId);
+			log.print("Sent new configs to rasPi with ID " + rasPiId);
+			System.exit(0);
+		}
+		else
+		{
+			log.write(false, "[ERROR] Network-NetworkServerSendCondfig; Could not send file to rasPi with ID: " + rasPiId);
+			log.print("Could not send file to rasPi with ID: " + rasPiId);
+			System.exit(8);
 		}
 	}
 
@@ -117,7 +115,13 @@ public class NetworkServerSendConfigs
 	private static void readInput (String[] args)
 	{
 		debugMode = false;
-		dummyMode = false; //TODO rm
+		
+		if (args.length == 0)
+		{
+			System.out.println("ERROR: You must write arguments" );
+			System.exit(7);
+		}
+		
 		for (int i=0; i < args.length; i++)
 		{	
 			if (args[i].equals("-h") || args[i].equals("--help"))
@@ -143,17 +147,13 @@ public class NetworkServerSendConfigs
 				i++;
 				serverPassword = args[i];
 			}
-			else if (args[i].equals("-m") || args[i].equals("--dummyMode"))
-			{
-				dummyMode = true;
-			}
 			else
 			{
 				System.out.println("ERROR: \"" + args[i] + "\" Unknow command");
 				System.exit(2);
 			}
 			
-			if (rasPiId == "")
+			if (rasPiId.equals(""))
 			{
 				System.out.println("ERROR: You must specified rasPi Id" );
 				System.exit(5);
@@ -172,7 +172,6 @@ public class NetworkServerSendConfigs
 		System.out.println(" -r OR --rasPiId     'String' :The rasPiId. Must be specified");
 		System.out.println(" -l OR --lectureHall 'String  :The new lecture hall");
 		System.out.println(" -p OR --password    'String' :New server password");
-		System.out.println(" -m OR --dummyMode            :Dummy mode wont send files only error numbers");
 		System.exit(0);
 	}
 	
