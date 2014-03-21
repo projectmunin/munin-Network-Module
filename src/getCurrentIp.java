bf7f1af921a8eb0320d94b7a2aac05e67764f52c
pandersson06 <pontus_proffs06@hotmail.com>
2014-03-21 07:57:11 +0100

    Well den behövdes :P

---
+++ src/getCurrentIp.java
@@ -0,0 +1,34 @@
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
+
+
+/**
+* Get the current ip address for the computer. Uses linux commands so
+* dont use this class when not on a linux computer
+* @author P. Andersson
+*
+*/
+public class getCurrentIp
+{
+	public String get (String ethernetName)
+	{
+		try {
+			NetworkInterface ni = NetworkInterface.getByName(ethernetName);
+			Enumeration<InetAddress> inetAddresses = ni.getInetAddresses();
+	
+			while(inetAddresses.hasMoreElements()) {
+				InetAddress ia = inetAddresses.nextElement();
+				if(!ia.isLinkLocalAddress()) {
+					return ia.getHostAddress();
+				}
+			}
+			return "";
+		}
+		catch (SocketException e)
+		{
+			return "";
+		}
+	}
+}
